7bf0f329d7665902f8cd7f26cfc787f252ae9231
amirkhanhard <ak9789858@gmail.com>
2025-10-24 03:38:44 +0530

    updated files

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  verbose: true,
+  testEnvironment: 'jsdom', //  needed for DOM APIs
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest', //  enable TypeScript
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], //  load jest-dom
+  
+};
+
+export default config;
---
+++ src/__tests__/contact.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Contact from '../pages/Contact';
+
+
+test('renders the Hello component', () => {
+  //  render component
+  render(<Contact />);
+
+  //  test it appears
+  expect(screen.getByText('Contact Us')).toBeInTheDocument();
+});
